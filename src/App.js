ef51825f078288d7bd7988c00dcc22ad7346b095
wen <wen.sima@gmail.com>
2024-09-24 16:58:06 +1000

    fix http error

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
           );
           if (!res.ok) {
             throw new Error("Something went wrong with fetching movies");
